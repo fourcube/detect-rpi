fc644c4150aeafd6c9ed2ec5b9cbd2c447acbe87
Chris Grieger <chris@scolp.de>
2017-05-11 08:53:48 +0200

    feat: make usable on node 4.x

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const PI_MODEL_NO = [ 'BCM2709' ];
+var fs = require('fs');
+var PI_MODEL_NO = [ 'BCM2709' ];
 
 function isPi (model) {
   return PI_MODEL_NO.indexOf(model) > -1;
 }
 
 module.exports = function () {
-  let cpuInfo;
+  var cpuInfo;
   try {
     cpuInfo = fs.readFileSync('/proc/cpuinfo', { encoding: 'utf8' });
   } catch (e) {
@@ -14,19 +14,19 @@ module.exports = function () {
     return false;
   }
 
-  const model = cpuInfo
+  var model = cpuInfo
     .split('\n')
     .map(line => line.replace(/\t/g, ''))
     .filter(line => line.length > 0)
     .map(line => line.split(':'))
     .map(pair => pair.map(entry => entry.trim()))
-    .filter(pair => pair[0] === 'Hardware') 
+    .filter(pair => pair[0] === 'Hardware')
   
   if(!model || model.length == 0) {
     return false;
   } 
 	
-  const number =  model[0][1];
+  var number =  model[0][1];
   return isPi(number);
 }
 
